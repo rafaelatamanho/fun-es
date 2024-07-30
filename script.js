6e360ff6561b3c4a30cdffa6adc8e1cbc493f68f
rafaelatamanho <108340588+rafaelatamanho@users.noreply.github.com>
2024-07-30 11:05:10 -0300

    30/07

---
+++ script.js
@@ -0,0 +1,7 @@
+const caixaPrincipal = document.queryselector("caixa-principal");
+const caixaPerguntas  = document.queryselector("caixa-perguntas");
+const caixaAlternativas = document.queryselector("caixa-alternativas");
+const caixaResultado = document.queryselector("caixa-resultado");
+const textoResultado = document.queryselector("texto-resultado");
+
+const perguntas = ["Pergunta1"]
